eadb011b13f0d46ffcb861a7bfd4a608ad937d20
mohamed bourra <itzmohamed007@gmail.com>
2024-05-11 23:29:30 +0100

    registered plan router in main application router

--- src/trpc/router.ts
+++ src/trpc/router.ts
@@ -3,12 +3,14 @@ import { router } from "./core";
 import { auth } from "../modules/auth/router";
 import { account } from "../modules/users/router";
 import { teams } from "../modules/teams/router";
+import { plans } from "../modules/plans/router";
 
 export const appRouter = router({
   auth,
+  plans,
   // protected
   account,
-  teams,
+  teams
 });
 
 export type AppRouter = typeof appRouter;
