f22fd1793c6c07cfecf647217078d9d5e40f23f9
mohamed bourra <itzmohamed007@gmail.com>
2024-05-13 00:09:43 +0100

    added plan update procedure

--- src/modules/plans/router.ts
+++ src/modules/plans/router.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { publicProcedure, router, trpcError } from "../../trpc/core";
 import db, { schema } from "../../db/client";
+import { eq } from "drizzle-orm";
 
 export const plans = router({
   create: publicProcedure.input(
@@ -13,21 +14,43 @@ export const plans = router({
       const { name, price } = input
       const plan = await db.query.plans.findFirst({});
       // plans duplication check
-      if(plan) throw new trpcError({
+      if (plan) throw new trpcError({
         code: "BAD_REQUEST"
       })
       // create plan
       const [createdPlan] = await db
-      .insert(schema.plans)
-      .values({
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        name,
-        price
-      })
-      .returning();
+        .insert(schema.plans)
+        .values({
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          name,
+          price
+        })
+        .returning();
       return {
         success: true
       };
+    }),
+  update: publicProcedure.input(
+    // validating params
+    z.object({
+      id: z.number(),
+      name: z.string(),
+      price: z.number()
+    }))
+    .mutation(async ({ input }) => {
+      const { name, price, id } = input;
+      const dbPlan = db.query.plans.findFirst({
+        where: eq(schema.plans.id, id)
+      });
+      if (!dbPlan) throw new trpcError({
+        code: "NOT_FOUND"
+      })
+      await db.update(schema.plans)
+        .set({ name, price })
+        .where(eq(schema.plans.id, id));
+      return {
+        success: true
+      }
     })
 })
