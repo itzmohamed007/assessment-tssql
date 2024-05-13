31e90f74a259367385dace0069be0dc325dbe199
mohamed bourra <itzmohamed007@gmail.com>
2024-05-13 17:52:34 +0100

    added plan prorated upgrate price procedure test

--- src/tests/plan/plan.test.ts
+++ src/tests/plan/plan.test.ts
@@ -3,6 +3,7 @@ import resetDb from "../helpers/resetDb";
 import { createCaller } from "../helpers/utils";
 import { db, schema } from "../../db/client";
 import { eq } from "drizzle-orm";
+import { trpcError } from "../../trpc/core";
 
 describe("plans routes", async () => {
   beforeAll(async () => {
@@ -48,6 +49,35 @@ describe("plans routes", async () => {
     it("should at lest get one plan", async () => {
       const results = await createCaller({}).plans.read();
       expect(results.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe("upgradePrice", () => {
+    const standardPlan = {
+      price: 9.99
+    }
+    const premiumPlan = {
+      price: 19.99
+    }
+
+    it("should throw an exception if old plan price is higher than the new plan`s", async () => {
+      await expect(createCaller({}).plans.upgradePrice({
+        newPlan: standardPlan,
+        oldPlan: premiumPlan
+      })).rejects.toThrowError(
+        new trpcError({
+          code: "BAD_REQUEST",
+        })
+      );
+    });
+
+    it("should return the prorated upgrade price", async () => {
+      const results = createCaller({}).plans.upgradePrice({
+        newPlan: premiumPlan,
+        oldPlan: standardPlan
+      });
+
+      expect((await results).price).toBeGreaterThan(0);
     });
   });
 });
